f24cefef56a15131da6c041632d2093dba449f07
Meg Briers <49128028+MegBriers@users.noreply.github.com>
2022-05-02 21:18:10 +0100

    once again fixing confusion with radians/degrees

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def value_generator(period, trig_function, months):
         # dealing with the reset for the later values
         if t == 6 and period == 60:
             t = 0
-        # WEIRD FLOATING POINT ERRORS WILL MESS IT UP
+        # WEIRD FLOATING POINT ERRORS WILL COMPOUND
         cur_vals.append(month * trig_function(math.radians(period * t)))
         t += 1
     return 2 * sum(cur_vals) / len(cur_vals)
@@ -37,11 +37,12 @@ def f(A0, a1, b1, a2, b2):
 
 
 def f_shifted(A0, A1, e1, A2, e2):
+    print(A0, A1, e1, A2, e2)
     results = []
     for i in range(12):
         two_pi_value = math.radians(30 * i)
         four_pi_value = math.radians(60 * i)
-        results.append(A0 - A1 * math.sin(two_pi_value + e1) + A2 * math.sin(four_pi_value + 32))
+        results.append(A0 - A1 * math.sin(two_pi_value + math.radians(e1)) + A2 * math.sin(four_pi_value + math.radians(e2)))
     return results
 
 
@@ -53,13 +54,6 @@ if __name__ == "__main__":
             print(filename)
 
             data = []
-
-            location_from = ""
-            location_to = ""
-            latitude = ""
-            longitude = ""
-            measuring = ""
-            year = ""
             meta_info = []
 
             for i, line in enumerate(file):
@@ -76,6 +70,7 @@ if __name__ == "__main__":
             results = f(A0, a1, b1, a2, b2)
 
             A1, A2, e1, e2 = shift_terms(a1, b1, a2, b2)
+
             results_shifted = f_shifted(A0, A1, e1, A2, e2)
 
             fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
@@ -84,8 +79,7 @@ if __name__ == "__main__":
 
             fig.supxlabel("months")
             fig.supylabel(meta_info[0])
-            # needs changed to new meta_info values
-            fig.suptitle(location_from + " to " + location_to + " " + year + " " + latitude + " " + longitude)
+            fig.suptitle(meta_info[1] + " to " + meta_info[2] + " " + meta_info[5] + " " + meta_info[3] + " " + meta_info[4])
             axes[1].set_title("Shifted")
             axes[0].set_title("Original")
 
